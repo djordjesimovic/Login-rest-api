4b59cda1fe085ee72e5ecc7f0db79311a87d153d
djordjesimovic <djordjesimovic.ds@gmail.com>
2023-01-17 00:16:36 +0100

    database update

--- dbConnection.js
+++ dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 const db_connection = mysql
   .createConnection({
-    host: "localhost", // HOST NAME
-    user: "root", // USER NAME
-    database: "node_auth_api", // DATABASE NAME
-    password: "bazapodataka7688", // DATABASE PASSWORD
+    host: "sql11.freesqldatabase.com", // HOST NAME
+    user: "sql11591185", // USER NAME
+    database: "sql11591185", // DATABASE NAME
+    password: "G3rQl2FXIr", // DATABASE PASSWORD
   })
   .on("error", (err) => {
     console.log("Failed to connect to Database - ", err);
